9bffce77905955f97db64d60d64a84d66f6db2e4
Habaki111 <Habaki001@gmail.com>
2023-05-01 16:28:59 +0700

    meta1

--- navbar/navbar.php
+++ navbar/navbar.php
@@ -5,30 +5,91 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style_navbar.css">
+    <link rel="stylesheet" href="./navbar/style.css">
 </head>
 
 <body>
-    <header class="header">
-        <nav>
-            <img class="logo" src="home.svg" alt="">
-            <ul class="nav_link">
-                <li><a href="#">SHOP</a></li>
-                <li><a href="#">SHOP</a></li>
-                <li><a href="#">SHOP</a></li>
-                <li><a href="#">SHOP</a></li>
-                <?php
-                if(true) { ?>
-                <li><a href="#">Admin</a></li>
-                <li><a href="#">Admin</a></li>
-                <li><a href="#">Admin</a></li>
-                <li><a href="#">Admin</a></li>
+    <nav>
+        <div>
+            <a href="#">Home</a>
+        </div>
 
-                <?php } ?>
-            </ul>
-            <button>submit</button>
-        </nav>
-    </header>
+        <div>
+            <a href="#">about</a>
+        </div>
+
+        <div>
+            <form>
+                <input type="text" name="search" placeholder="ค้นหา">
+                <button>search</button>
+            </form>
+        </div>
+
+    </nav>
+    <div class="shadow"></div>
 </body>
 
 </html>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <form action="login_back.php" method="post">
+        <div>
+            <h1>Login</h1>
+        </div>
+
+        <div>
+            <label for="email">Email</label>
+            <input type="text" name="username">
+        </div>
+
+        <div>
+            <label for="password">Password</label>
+            <input type="password" name="password">
+        </div>
+        <p>
+            <button>submit</button>
+        </p>
+
+        <p>
+            <a href="register.php">resister</a>
+        </p>
+    
+    
+    </form>
+</body>
+</html>
+
+<style>
+    nav {
+    display: flex;
+    margin: 0 auto;
+    padding: 5px 0 0 0;
+    justify-content: center;
+    align-items: center;
+}
+
+div {
+    margin: 2px;
+    padding: 6px;
+}
+
+a {
+    font-size: 20px;
+    text-decoration: none;
+}
+
+.shadow {
+    box-shadow: 0px 3px 0px #6ed6ff;
+    padding: 1px;
+    margin: 1px;
+}
+</style>
--- register.php
+++ register.php
@@ -1,40 +1,46 @@
+<?php
+    session_start();    
+    if(isset($_POST['submit'])) {
+        require("./db.php");
+        $res = $db->query("SELECT * FROM `usr` WHERE usr_username = '{$_POST['username']}' ");
+        if($res->num_rows == 0) {
+            $db->query("INSERT INTO `usr`(`usr_id`, `usr_name`, `usr_username`, `usr_password`) VALUES 
+            (null,'{$_POST['name']}','{$_POST['username']}','{$_POST['password']}')");
+            $msg = '</div>register success<div>';
+        }else{
+            $msg = '</div>this username has already used<div>';
+        }
+
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Register</title>
+    <title>Document</title>
 </head>
 <body>
-    <form action="register_back.php" method="post">
+    <?php require('./navbar/navbar.php'); ?>
+    <?php echo $msg ?? ""; ?>
+    <form method="post">
         <div>
-            <h1>Register</h1>
+            <label for="">name</label>
+            <input type="text" name="name" id="name">
         </div>
-
         <div>
-            <label for="name">Name</label>
-            <input type="text" name="name">
-        </div>
-
-        <div>
-            <label for="email">Email</label>
+            <label for="username">username</label>
             <input type="text" name="username">
         </div>
-
         <div>
-            <label for="password">Password</label>
+            <label for="password">password</label>
             <input type="password" name="password">
         </div>
-        <p>
-            <button>submit</button>
-        </p>
-
-        <p>
-            <a href="login.php">Login</a>
-        </p>
-    
-    
+        <div>
+            <button name="submit" >clik</button>
+        </div>
     </form>
 </body>
-</html>
+</html> 
